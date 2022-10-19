a1b08c2ba6732f81df67095206c2de389921e492
278deco <278deco.officiel@gmail.com>
2022-10-19 11:29:31 +0200

    Added all the set of test for RandomSeed

--- src/test/java/fr/f1parking/core/level/gen/RandomSeedTest.java
+++ src/test/java/fr/f1parking/core/level/gen/RandomSeedTest.java
@@ -1,14 +1,32 @@
 package fr.f1parking.core.level.gen;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-class RandomSeed {
+import fr.f1parking.core.level.gen.RandomSeed.RandomSeedByte;
+
+class RandomSeedTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		final RandomSeed definedSeed = new RandomSeed(5461684566155L);
+		
+		//Two seed shouldn't be able to have the same value
+		//Dangerous test because a tiny probability could make two seed equals
+		assertNotEquals(new RandomSeed().getSeed(), new RandomSeed().getSeed());
+		
+		//When a seed is set in the constructor, it set the main seed of the class with the same value
+		assertEquals(definedSeed.getSeed(), 5461684566155L);
+		
+		//Cannot generate a seed without a byte weight
+		assertThrows(IllegalArgumentException.class, () -> new RandomSeed(RandomSeedByte.NOT_GENERATED));
+
+		//Check if the equals function works
+		assertEquals(new RandomSeed(5461684566155L).equals(definedSeed), true);
+
 	}
 
 }
