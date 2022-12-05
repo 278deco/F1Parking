cc3dfbbae87240aa38a730e9737d1cd3eba57383
278deco <278deco.officiel@gmail.com>
2022-12-06 00:41:55 +0100

    Made change to the main class

--- src/main/java/fr/f1parking/core/Main.java
+++ src/main/java/fr/f1parking/core/Main.java
@@ -1,39 +1,29 @@
 package fr.f1parking.core;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import fr.f1parking.core.entities.Car;
-import fr.f1parking.core.entities.Entity;
-import fr.f1parking.core.entities.EntityPlayer;
-import fr.f1parking.core.entities.Truck;
-import fr.f1parking.core.entities.placement.Direction;
-import fr.f1parking.core.level.Map;
+import fr.f1parking.core.io.IOHandler;
+import fr.f1parking.core.level.Difficulty;
 import fr.f1parking.core.level.gen.RandomSeed;
-import fr.f1parking.core.level.gen.RandomSeed.RandomSeedByte;
-import fr.f1parking.core.level.gen.generators.ManualPlacingGenerator;
-import fr.f1parking.core.level.gen.layers.ManualPlacingLayer;
-import fr.f1parking.core.utils.EntitiesTexture;
+import fr.f1parking.core.level.gen.backtracking.Tree;
 
 public class Main {
 	
-	private static EntityPlayer player;
-	private static List<Entity> entities;
-	private static Map map;
+//	private static EntityPlayer player;
+//	private static List<Entity> entities;
+//	private static Map map;
+	
+	private static Tree treeMap;
 	
 	private static final Logger LOGGER = LogManager.getLogger(Main.class);
 	
 	public static void main(String[] args) {
-		RandomSeed rdm = new RandomSeed(RandomSeedByte.BYTE_64);
-
-		LOGGER.info(rdm +" "+ Long.bitCount(rdm.getSeed()));
+		IOHandler.getInstance();
 		
-//		generateMap();
-//			
-//		map.testRender(entities, player);
+		generateTreeMap();
+		
+		treeMap.testRender();
 //		
 //		LOGGER.info(map.moveEntity(player, Direction.WEST));
 //		
@@ -55,10 +45,19 @@ public class Main {
 //				
 //		map.testRender(entities, player);
 //		
-////		LOGGER.info(map.moveEntity(entities.get(1), Direction.SOUTH));
-////				
-////		map.testRender(entities, player);
+//		LOGGER.info(map.moveEntity(entities.get(1), Direction.SOUTH));
+//				
+//		map.testRender(entities, player);
 	
+	}
+	
+	public static void generateTreeMap() {
+		treeMap = Tree.builder()
+			.seed(new RandomSeed())
+			.difficulty(Difficulty.EASY)
+			.build();
+		
+		treeMap.generateMap();
 	}
 	
 	public static byte byteGen(byte obj, byte size, byte rot) {
@@ -73,31 +72,31 @@ public class Main {
 		return new byte[] {obj,size,rot};
 	}
 	
-	public static void generateMap() {
-		player = new EntityPlayer();
-		entities = new ArrayList<>();
-		generateEntities(entities);
-		
-		ManualPlacingGenerator generator = ManualPlacingGenerator.builder()
-				.placement(4, 2, 2, 2, Direction.EAST)
-				.placement(0, 0, 1, 2, Direction.EAST)
-				.placement(1, 5, 1, 3, Direction.NORTH)
-				.build();
-		
-		ManualPlacingLayer layer = ManualPlacingLayer.builder()
-				.entities(entities).player(player)
-				.build();
-		
-		map = Map.builder()
-				.generator(generator)
-				.layer(layer)
-				.name("Test Map")
-				.build();
-		
-	}
-	
-	private static void generateEntities(List<Entity> eList) {
-		eList.add(new Car(Direction.EAST, EntitiesTexture.CAR_1_TEXTURE));
-		eList.add(new Truck(Direction.NORTH, EntitiesTexture.TRUCK_1_TEXTURE));
-	}
+//	public static void generateMap() {
+//		player = new EntityPlayer();
+//		entities = new ArrayList<>();
+//		generateEntities(entities);
+//		
+//		ManualPlacingGenerator generator = ManualPlacingGenerator.builder()
+//				.placement(4, 2, 2, 2, Direction.EAST)
+//				.placement(0, 0, 1, 2, Direction.EAST)
+//				.placement(2, 2, 1, 3, Direction.NORTH)
+//				.build();
+//		
+//		ManualPlacingLayer layer = ManualPlacingLayer.builder()
+//				.entities(entities).player(player)
+//				.build();
+//		
+//		map = Map.builder()
+//				.generator(generator)
+//				.layer(layer)
+//				.name("Test Map")
+//				.build();
+//		
+//	}
+//	
+//	private static void generateEntities(List<Entity> eList) {
+//		eList.add(new Car(Direction.EAST, EntitiesTexture.CAR_1_TEXTURE));
+//		eList.add(new Truck(Direction.NORTH, EntitiesTexture.TRUCK_1_TEXTURE));
+//	}
 }
